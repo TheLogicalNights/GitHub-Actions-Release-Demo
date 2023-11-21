3fe9135419c2536eb10dfa79e4000b805ee9d357
Swapnil Ramesh Adhav <swapniladhav560@gmail.com>
2023-11-21 17:48:29 +0530

    Initial commit

---
+++ src/main/java/com/example/GitHubActionsReleaseDemo/GitHubActionsReleaseDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.GitHubActionsReleaseDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsReleaseDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubActionsReleaseDemoApplication.class, args);
+	}
+
+}
