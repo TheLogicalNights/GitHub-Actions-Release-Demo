138b5f7b885b80f7874133b4db4eff62d7261bea
Swapnil Ramesh Adhav <swapniladhav560@gmail.com>
2023-11-22 15:19:26 +0530

    Release v1.0.5

--- src/main/java/com/example/GitHubActionsReleaseDemo/GitHubActionsReleaseDemoApplication.java
+++ src/main/java/com/example/GitHubActionsReleaseDemo/GitHubActionsReleaseDemoApplication.java
@@ -1,13 +1,25 @@
 package com.example.GitHubActionsReleaseDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class GitHubActionsReleaseDemoApplication {
+    private static final Logger logger = LoggerFactory.getLogger(GitHubActionsReleaseDemoApplication.class);
+    public static void main(String[] args) {
+		ConfigurableApplicationContext ctx = SpringApplication.run(GitHubActionsReleaseDemoApplication.class, args);
 
-	public static void main(String[] args) {
-		SpringApplication.run(GitHubActionsReleaseDemoApplication.class, args);
-	}
+        String username = System.getenv("USERNAME");
+        String password = System.getenv("PASSWORD");
 
+
+        logger.info("[Username: " + username + "]");
+        logger.info("[Password: " + password + "]");
+
+		ctx.getBean(GitHubActionsReleaseDemoApplication.class);
+		ctx.close();
+    }
 }
